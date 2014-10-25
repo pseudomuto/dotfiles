6ae6b5f1dbfca6884716bcd354ac7d31c5018852
pseudomuto <david.muto@gmail.com>
2014-10-25 16:04:27 -0400

    simple setup for osx

--- setup/downloads.sh
+++
@@ -1,9 +0,0 @@
-curl -L https://cachefly.alfredapp.com/Alfred_2.5.1_308.zip > ~/Downloads/alfredapp.zip
-curl -L http://downloads.binaryage.com/TotalFinder-1.6.12.dmg > ~/Downloads/totalfinder.dmg
-curl -L https://iterm2.com/downloads/stable/iTerm2_v2_0.zip > ~/Downloads/iterm2.zip
-curl -L http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203065.dmg >
-~/Downloads/sublime.dmg
-curl -L https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.5.dmg > ~/Downloads/vagrant.dmg
-
-# Chrome
-open https://www.google.ca/chrome/browser/#eula
--- setup/rubies.sh
+++
@@ -1,3 +0,0 @@
-mkdir ~/.rubies
-ruby-build 2.1.2 ~/.rubies/2.1.2
-ruby-build 2.1.3 ~/.rubies/2.1.2
