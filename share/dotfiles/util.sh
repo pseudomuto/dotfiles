0f1071427d16b281773490c198d70ee3247ca228
David Muto (pseudomuto) <david.muto@gmail.com>
2016-08-02 13:39:47 -0400

    Standardizing task options

--- share/dotfiles/util.sh
+++ share/dotfiles/util.sh
@@ -3,4 +3,5 @@ set -euo pipefail
 source "${BASH_SOURCE[0]%/*}/util/console.sh"
 source "${BASH_SOURCE[0]%/*}/util/downloader.sh"
 source "${BASH_SOURCE[0]%/*}/util/ipc.sh"
+source "${BASH_SOURCE[0]%/*}/util/options.sh"
 source "${BASH_SOURCE[0]%/*}/util/package_manager.sh"
