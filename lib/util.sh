45d2da55937d5e0e3c4005bdcdcc32940d7e38b7
David Muto (pseudomuto) <david.muto@gmail.com>
2016-08-05 22:26:16 -0400

    Make symlinks work on osx

--- lib/util.sh
+++ lib/util.sh
@@ -9,6 +9,10 @@ osx() {
   [ "$(uname)" == "Darwin" ]
 }
 
+real_path() {
+  echo $(perl -e 'use Cwd "abs_path";print abs_path(shift)' $1)
+}
+
 source "${BASH_SOURCE[0]%/*}/util/console.sh"
 source "${BASH_SOURCE[0]%/*}/util/downloader.sh"
 source "${BASH_SOURCE[0]%/*}/util/ipc.sh"
