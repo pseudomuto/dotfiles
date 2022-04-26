193e1034fa6037e0c6ab9b39a8d32262f62498c9
David Muto (pseudomuto) <david.muto@gmail.com>
2022-04-26 09:32:23 -0400

    Install cockroach on OSX as well

--- apps/cockroachdb.nix
+++ apps/cockroachdb.nix
@@ -1,7 +1,9 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, isLinux, ... }:
 let
+  arch = if isLinux then "linux" else "darwin-10.9";
+  version = "21.2.9";
   crdb = builtins.fetchTarball{
-    url = "https://binaries.cockroachdb.com/cockroach-v21.2.5.linux-amd64.tgz";
+    url = "https://binaries.cockroachdb.com/cockroach-v${version}.${arch}-amd64.tgz";
   };
 in
 {
